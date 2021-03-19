9d98c72f381b7ff3d4fdd3815932d054e3b99a1b
hyeonho1 <80078810+hyeonho1@users.noreply.github.com>
2021-03-19 23:21:49 +0900

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+try:
+    korea = int (input(90))
+    Math = int(intput(50))
+    Engilsh = int(input(60))
+    print('성적이 입렸됬었습니다')
+    score = Korea + Mth + Engilsh
+    average = sum(score)/len(score)
+    
+    except:
+        print("Error")
+        else:
+            if average > 100 or grade < 0:
+                print("Error")
+            elif average>=90:
+                print(average,'A')
+            elif average>=50:
+                print(average,'B')
+            elif averager>=60:
+                print(average,'C')
+            
